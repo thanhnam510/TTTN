0af7e1a715f93c45850381fb64b048951c2a5770
thanhnam510 <thanhnam510@gmail.com>
2018-08-09 02:16:19 +0700

    09082018

--- EmoDictionary/EmoDic/DuLieuChuanHoa.cs
+++ EmoDictionary/EmoDic/DuLieuChuanHoa.cs
@@ -13,7 +13,7 @@ namespace EmoDic
         public double TG { get; set; }
         public double CL { get; set; }
         public double TB { get; set; }
-        static DuLieuChuanHoa PCentroid = new DuLieuChuanHoa(0.5948,1.0086,0.1724,0.181,0.1296);
+        static DuLieuChuanHoa PCentroid = new DuLieuChuanHoa(0.5948,1.0086,0.1724,0.181,0.1293);
         static DuLieuChuanHoa NCentroid = new DuLieuChuanHoa(-0.4091,-0.4886,-0.2727,-0.0909,-0.0568);
         static DuLieuChuanHoa NeuCentroid = new DuLieuChuanHoa(-0.02,-0.08,-0.08,0.08,0.1);
 
@@ -43,30 +43,16 @@ namespace EmoDic
                 return 0;
             if (khiacanh.Count == 1)
                 return khiacanh[0].trongso;
-            List<float> tieucuc = new List<float>();
-            List<float> tichcuc = new List<float>();
-            foreach (CamXuc cx in khiacanh)
+            List<float> listTS = new List<float>();
+            foreach(CamXuc cx in khiacanh)
             {
-                if (cx.trongso < 0)
-                    tieucuc.Add(cx.trongso);
-                else
-                    tichcuc.Add(cx.trongso);
+                listTS.Add(cx.trongso);
             }
-            int NCount = tieucuc.Count;
-            int PCount = tichcuc.Count;
-            if (tieucuc.Count == 0)
-                return tichcuc.Max();
-            if (tichcuc.Count == 0)
-                return tieucuc.Min();
-                for (int i = 0; i < PCount || i < NCount; i++)
-                {
-                float result = tichcuc[PCount - i] + tieucuc[i];
-                if (result != 0)
-                    return result;
-                }
-            int a = PCount - NCount;
-            if (a == 0) return 0;
-            return a<0? tieucuc[PCount] : tichcuc[PCount - a];
+            float kq = listTS.Min() + listTS.Max();
+            if (kq == 0) return 0;
+            return kq < 0 ? listTS.Min() : listTS.Max();
+
+
             
 
         }
